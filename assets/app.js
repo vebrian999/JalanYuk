7c39a7b917a2b175f24b6a9bca945108212f09c1
vebrian 2019 <vebrian999@gmail.com>
2024-02-20 01:05:13 +0700

    perbaikan responsif dan jumbotron

--- assets/app.js
+++ assets/app.js
@@ -1,13 +1,37 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var currentYear = new Date().getFullYear();
-  document.getElementById("year").innerText = currentYear;
-});
+  // Set the current year
+  document.getElementById("year").innerText = new Date().getFullYear();
 
-document.addEventListener("DOMContentLoaded", function () {
+  // Mobile menu functionality
   const mobileMenuButton = document.getElementById("mobile-menu");
   const navList = document.querySelector("nav ul");
 
   mobileMenuButton.addEventListener("click", function () {
     navList.classList.toggle("show");
   });
+
+  // Navbar background change on scroll
+  const nav = document.querySelector("nav");
+
+  window.addEventListener("scroll", function () {
+    nav.classList.toggle("scrolled", window.scrollY > 50);
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const jumbotron = document.querySelector(".jumbotron");
+  const images = ["./assets/img/header-bg (1).png", "./assets/img/header-bg (2).png", "./assets/img/header-bg (3).png", "./assets/img/header-bg (4).png"];
+  let currentImageIndex = 0;
+
+  function changeBackgroundImage() {
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    const imageUrl = `url('${images[currentImageIndex]}')`;
+    jumbotron.style.backgroundImage = imageUrl;
+  }
+
+  // Call the function initially
+  changeBackgroundImage();
+
+  // Set interval to change the background image every few seconds
+  setInterval(changeBackgroundImage, 5000); // Change 5000 to the interval you desire (in milliseconds)
 });
