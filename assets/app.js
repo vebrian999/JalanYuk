3008489c222a5c227efddbb25975547a1855dc7b
vebrian 2019 <vebrian999@gmail.com>
2024-02-20 22:53:08 +0700

    perbaikan responsif, jumbotron dan rapiin 3

--- assets/app.js
+++ assets/app.js
@@ -16,22 +16,52 @@ document.addEventListener("DOMContentLoaded", function () {
   window.addEventListener("scroll", function () {
     nav.classList.toggle("scrolled", window.scrollY > 50);
   });
-});
 
-document.addEventListener("DOMContentLoaded", function () {
-  const jumbotron = document.querySelector(".jumbotron");
-  const images = ["./assets/img/header-bg (1).png", "./assets/img/header-bg (2).png", "./assets/img/header-bg (3).png", "./assets/img/header-bg (4).png"];
-  let currentImageIndex = 0;
+  // Highlight the active link based on the current section
+  function highlightLink(sectionId, linkId) {
+    const section = document.getElementById(sectionId);
+    const link = document.getElementById(linkId);
 
-  function changeBackgroundImage() {
-    currentImageIndex = (currentImageIndex + 1) % images.length;
-    const imageUrl = `url('${images[currentImageIndex]}')`;
-    jumbotron.style.backgroundImage = imageUrl;
+    function isSectionInView() {
+      const rect = section.getBoundingClientRect();
+      return rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2;
+    }
+
+    function handleScroll() {
+      if (isSectionInView()) {
+        link.classList.add("active");
+      } else {
+        link.classList.remove("active");
+      }
+    }
+
+    // Check on page load
+    handleScroll();
+
+    // Check when scrolling
+    window.addEventListener("scroll", handleScroll);
   }
 
-  // Call the function initially
-  changeBackgroundImage();
-
-  // Set interval to change the background image every few seconds
-  setInterval(changeBackgroundImage, 5000); // Change 5000 to the interval you desire (in milliseconds)
+  // Highlight the links for different sections
+  highlightLink("kategori", "kategori-link");
+  highlightLink("featured", "featured-link");
+  // highlightLink("AboutMe", "AboutMe-link");
+  highlightLink("blog", "blog-link");
 });
+
+// Background image change for jumbotron
+const jumbotron = document.querySelector(".jumbotron");
+const images = ["./assets/img/header-bg (1).png", "./assets/img/header-bg (2).png", "./assets/img/header-bg (3).png", "./assets/img/header-bg (4).png"];
+let currentImageIndex = 0;
+
+function changeBackgroundImage() {
+  currentImageIndex = (currentImageIndex + 1) % images.length;
+  const imageUrl = `url('${images[currentImageIndex]}')`;
+  jumbotron.style.backgroundImage = imageUrl;
+}
+
+// Call the function initially
+changeBackgroundImage();
+
+// Set interval to change the background image every few seconds
+setInterval(changeBackgroundImage, 5000); // Change 5000 to the interval you desire (in milliseconds)
